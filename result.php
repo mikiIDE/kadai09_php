3e91587e9c713b16e54ade97723f8f059f98764e
mikiIDE <kinoko.miki@gmail.com>
2024-11-20 16:36:23 +0900

    update css and result.php

--- result.php
+++ result.php
@@ -12,11 +12,23 @@
     <div class="container">
         <h2>投票結果</h2>
         <?php
+        function showRating($value,$symbol){
+            $output="";
+            for ($i = 0; $i < 5; $i++){
+                if($i < $value){
+                    $output.= '<span class="rating-symbol filled">'.$symbol.'</span>';
+                }else{
+                    '<span class="rating-symbol">' . $symbol . '</span>';
+                }
+            }
+            return $output;
+        }
+
         // ファイルの読み込み
         $file = fopen("data/data.csv", "r"); // r = 読み取り専用
 
         // テーブルのヘッダーを作る
-        echo '<table border="1">';
+        echo '<table>';
         echo '<tr>';
         echo '<th>お名前</th>';
         echo '<th>メールアドレス</th>';
@@ -30,12 +42,18 @@
         // csvのデータを配列に変換し、HTMLに埋め込む
         while ($line = fgetcsv($file)) {
             echo "<tr>";
-            for ($i = 0; $i < count($line); $i++) {
+            //名前、メール、タグのみ表示
+            for ($i = 0; $i < 3; $i++) {
                 echo "<td>" . htmlspecialchars($line[$i]) . "</td>"; //htmlspecialcharsはセキュリティの観点からつけたほうがいい
             }
+            // 評価を記号で表示
+            echo "<td>" . showRating($line[3], '★') . "</td>";
+            echo "<td>" . showRating($line[4], '♥') . "</td>";
+            echo "<td>" . showRating($line[5], '☺') . "</td>";
+            // コメントを表示
+            echo "<td>" . htmlspecialchars($line[6]) . "</td>";
             echo "</tr>";
         }
-
         echo "</table>";
 
         // ファイルを閉じる
